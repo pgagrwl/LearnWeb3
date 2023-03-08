1611d6ad4a46e2218aeef9abc0ad9c9b4eab8b9f
pgagrwl <pgagrwl@gmail.com>
2023-03-08 17:55:15 +0530

    initial commit

---
+++ sophomore/full-nft-collection/hardhat-tutorial/README.md
@@ -0,0 +1,9 @@
+# NFT collection - Crypto Devs.
+
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat compile
+npx hardhat run scripts/deploy.js --network goerli
+```
---
+++ sophomore/full-nft-collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x14440cE753846B204bE1eAE9b54Bd186EEC39c85";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
