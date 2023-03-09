29bec0c84b10a51798b41c70edc06a646c60522b
pgagrwl <pgagrwl@gmail.com>
2023-03-09 23:47:05 +0530

    Sepolia network config added

--- sophomore/full-nft-collection/hardhat-tutorial/hardhat.config.js
+++ sophomore/full-nft-collection/hardhat-tutorial/hardhat.config.js
@@ -2,6 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -10,6 +12,10 @@ module.exports = {
   networks: {
     goerli: {
       url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
   },
