063fede0e0bfa3ffc43bfea509128e7f3f34cc53
pgagrwl <pgagrwl@gmail.com>
2023-03-11 13:10:49 +0530

    Initial commit

---
+++ sophomore/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x96BDd86030336187bE064aD7DF1f81a4813a174f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
